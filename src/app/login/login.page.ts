ea94885e5fb5df69e7c62c527c512d54a09759b0
phaalonso <phaalonso@gmail.com>
2020-12-22 21:19:22 -0300

    Pagina de login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AutenticacaoService } from '../service/autenticacao.service';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  public formulario: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private autenticacaoService: AutenticacaoService
+  ) {
+    this.formulario = formBuilder.group({
+      nome: ["", [Validators.required, Validators.minLength(5)]],
+      senha: ["", [Validators.required, Validators.minLength(5)]]
+    });
+  }
+
+  submit() {
+    if (!this.formulario.valid) {
+      return;
+    }
+
+    console.log(this.formulario.value);
+    this.autenticacaoService.logar(this.formulario.value);
+
+  }
 
   ngOnInit() {
   }
